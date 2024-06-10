f1c9b716c205f68607c8028777eec05251fe4280
alexandre.sgoncalves <g.aleprojetos.Repositories@gmail.com>
2024-06-10 11:45:27 -0300

    Initial commit

---
+++ Server/Entities/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Server.Entities
+{
+    public class Usuario : BaseEntity
+    {
+        public String Nome { get; set; }
+        public String Senha { get; set; }
+
+        public Usuario(string nome, string senha, string email)
+        {
+            Nome = nome;
+            Senha = senha;
+            Email = email;
+            CriadoEm = DateTime.UtcNow;
+        }
+
+        public void AtualizarUsuario(string nome, string senha, string email)
+        {
+            if (nome != null) Nome = nome;
+            if (senha != null) Senha = senha;
+            if (email != null) Email = email;
+            AtualizadoEm = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Server/V1/Schemas/Request/UsuarioRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server.Endpoints.UsuarioForm.request
+{
+    public class UsuarioRequest
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+    }
+}
